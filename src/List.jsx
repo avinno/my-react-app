6a0bccf05b3fde4cb0bda1bc0b48f28d2a268a24
Aaron Varga <aaronvarga@onmail.com>
2024-08-06 18:49:16 -0400

    jsx updates

--- src/List.jsx
+++ src/List.jsx
@@ -3,7 +3,17 @@ function List() {
                                                {id: 2, name: "orange", calories: 45},
                                                {id: 3, name: "banana", calories: 105},
                                                {id: 4, name: "pineapple", calories: 150}];
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li>);
+
+    // fruits.sort((a, b) => a.name.localCompare(b.name)); // Alphabetical
+    // fruits.sort((a, b) => b.name.localCompare(a.name)); // Reverse Alphabetical
+    // fruits.sort((a, b) => a.calories - b.calories); // Numeric Order
+    // fruits.sort((a, b) => b.calories - a.calories); // Reverse Numberic or Descending
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+
+    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; {fruit.calories} cal</li>);
+
     return(<ul>{listItems}</ul>);
 }
 
