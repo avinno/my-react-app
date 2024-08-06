9e54e35ed2412a7ed8134d522eaf9d41096bc318
Aaron Varga <aaronvarga@onmail.com>
2024-08-06 16:24:59 -0400

    jsx updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Footer from './Footer';
 import Food from './Food.jsx';
 import Card from './Card';
 import Student from './Student';
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
     return (
         <>
             <div className="max-w-md m-auto">
                 <Header />
+                <UserGreeting isLoggedIn={true} username="Apvarga" />
                 <Student firstName="Aaron" lastName="Varga" age={34} isStudent={true} />
                 <Student firstName="John" lastName="Doe" age={24} isStudent={false} />
                 <Student />
