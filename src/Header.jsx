53150df23372921da3950686f7b6f8106d63115f
Aaron Varga <aaronvarga@onmail.com>
2024-08-06 20:45:16 -0400

    jsx updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return(
         <header className="my-4 border-b border-gray-200 pb-2">
-            <h1 className="text-2xl font-bold mb-2">My Website</h1>
+            <h1 className="text-2xl font-bold mb-2">🤠 My Website</h1>
             <nav>
                 <ul className="flex flex-row gap-6 text-blue-500">
                     <li><a href="">Home</a></li>
