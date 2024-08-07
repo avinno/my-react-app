291f824a0bb69a1db2ef5d7e690376b9a7b333f9
Aaron Varga <aaronvarga@onmail.com>
2024-08-06 21:13:38 -0400

    jsx updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,11 +3,11 @@ function Header() {
         <header className="my-4 border-b border-gray-200 pb-2">
             <h1 className="text-2xl font-bold mb-2">🤠 My Website</h1>
             <nav>
-                <ul className="flex flex-row gap-6 text-blue-500">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Services</a></li>
-                    <li><a href="">Contact</a></li>
+                <ul className="flex flex-row text-blue-500">
+                    <li><a href="" className="p-4">Home</a></li>
+                    <li><a href="" className="p-4">About</a></li>
+                    <li><a href="" className="p-4">Services</a></li>
+                    <li><a href="" className="p-4">Contact</a></li>
             </ul>
             </nav>
         </header>
